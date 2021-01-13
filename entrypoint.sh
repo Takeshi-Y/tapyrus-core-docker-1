bce583e01937ef2e4b7a89d2d58bf6c962ef50ce
Takeshi Yasunaga <yasunaga@haw.co.jp>
2021-01-13 16:47:42 +0900

    set NETWORK_ID from environments.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ bind=0.0.0.0
 rpcallowip=0.0.0.0/0
 
 server=1
-keypool=1
 discover=0
 
 dev=1
@@ -19,7 +18,11 @@ networkid=1905960821
 EOS
 fi
 
-network_id=`cat ${CONF_DIR}/tapyrus.conf | grep networkid= | grep -oE "[0-9]+"`
+if [ -v NETWORK_ID ]; then
+  network_id=${NETWORK_ID}
+else
+  network_id=`cat ${CONF_DIR}/tapyrus.conf | grep networkid= | grep -oE "[0-9]+"`
+fi
 
 if [ -v GENESIS_BLOCK_WITH_SIG -a ! -e ${DATA_DIR}/genesis.${network_id} ]; then
   echo ${GENESIS_BLOCK_WITH_SIG} > ${DATA_DIR}/genesis.${network_id}
